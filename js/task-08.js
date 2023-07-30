ca8722af1c326acf73943c85973649706ffae8e2
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-07-30 17:46:53 +0300

    finishing solve tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onSubmit);
 
+function onSubmit(event) {
+    event.preventDefault();
+    // console.dir(event.currentTarget)
+    const {email, password} = event.currentTarget.elements;
+        
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(data)
+    formEl.reset()
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const button = document.querySelector(".change-color");
+const body =document.querySelector("body");
+const spanColorValue = document.querySelector(".color");
+
+button.addEventListener("click", onClick);
+
+function onClick (evt) {
+  let randClr =getRandomHexColor();
+  body.style.backgroundColor = randClr;
+  spanColorValue.textContent = randClr;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
